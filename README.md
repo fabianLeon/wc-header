128bf1528e6666f7f646c59228f1b4635cd1daa8
Fabián Sánchez León <fadarsaleeing@gmail.com>
2020-09-14 18:35:46 -0500

    webComponent up version

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# wc-header
-WebComponente using svelte :)
+## WebComponent header simple with svelte and use angular app
+
+1. Install
+
+```bash
+npm i wc-header
+```
+
+in angular.json add into scripts
+
+```bash
+ "scripts": [
+    ...
+    "node_modules/wc-header/public/build/bundle.js"
+    ...
+    ]
+```
+2. In app.module
+
+```typescript
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+...
+
+@NgModule({
+  ...
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
+})
+```
+3. Use into html. 
+```html
+<svelte-beauty-header 
+    name="Title application"
+    subname="Subtitle application"
+    description="Description ... "
+    button="Label Button"
+    active="true">
+</svelte-beauty-header>
+```
---
+++ src/App.svelte
@@ -0,0 +1,73 @@
+<svelte:options tag="svelte-beauty-header"></svelte:options>
+<script>
+	export let name;
+	export let subname;
+	export let description;
+	export let button;
+	export let active;
+</script>
+{#if !!active}
+<main>
+	<header>
+		<div class="overlay">
+	<h1>{name?name: ''}</h1>
+	<h3>{subname?subname: ''}</h3>
+	<p>{description?description: ''}</p>
+		<br>
+		<div>
+		{#if !!button}
+		<button>{button?button:''}</button>
+		{/if}
+		</div>
+	</header>
+</main>
+{/if}
+
+<style>
+	*{padding: 0; margin: 0; box-sizing: border-box;}
+	body{height: 900px;}
+	header {
+		text-align: center;
+		width: 100%;
+		height: auto;
+		background-size: cover;
+		background-attachment: fixed;
+		position: relative;
+		overflow: hidden;
+		border-radius: 0 0 85% 85% / 30%;
+	}
+	header .overlay{
+		width: 100%;
+		height: 100%;
+		padding: 50px;
+		color: #FFF;
+		text-shadow: 1px 1px 1px #333;
+	background-image: linear-gradient( 135deg, #9f05ff69 10%, #fd5e086b 100%);
+		
+	}
+
+	h1 {
+		font-family: 'Dancing Script', cursive;
+		font-size: 80px;
+		margin-bottom: 30px;
+	}
+
+	h3, p {
+		font-family: 'Open Sans', sans-serif;
+		margin-bottom: 30px;
+	}
+
+	button {
+		border: none;
+		outline: none;
+		padding: 10px 20px;
+		border-radius: 50px;
+		color: #333;
+		background: #fff;
+		margin-bottom: 50px;
+		box-shadow: 0 3px 20px 0 #0000003b;
+	}
+	button:hover{
+		cursor: pointer;
+	}
+</style>
